1460d44538a6c20d44a7bd603710d2e1e9ab0010
Luciano Pereira <127886247+lucianofullstack@users.noreply.github.com>
2024-01-27 12:51:01 +0100

    :octocat: debug

--- src/routes/lists.js
+++ src/routes/lists.js
@@ -1,14 +1,21 @@
 const
     { Router } = require('express'),
-    getter = require('../modules/getters'),
     router = Router()
+/*
+    getter = require('../modules/getters')
+*/
     
     router.get('*', async (req, res) => {
+/*
         const
             response = await getter.lists()
         if (response.code) {
             return res.status(response.code).json(response)
         }; return res.status(200).json(response)
+        */
+
+return res.status(200).json('test')
+
     })
     
     module.exports = router
